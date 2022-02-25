4bb3712d2a514805df35f5355e18517d8fe65d4d
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2022-02-24 19:46:55 -0500

    refactor(cron): move cron job to its own file.
    The cron job was placed in `/config/cron-tasks.js` to stay inline with strapi server structure

---
+++ server/config/cron-tasks.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const { getPluginService } = require('../utils/getPluginService');
+
+module.exports = {
+	registerCronTasks: ({ strapi }) => {
+		// create cron check
+		strapi.cron.add({
+			'*/1 * * * *': async ({ strapi }) => {
+				// fetch all actions that have passed
+				const records = await getPluginService(strapi, 'actionService').find({
+					filters: {
+						executeAt: {
+							$lte: new Date(),
+						},
+					},
+				});
+
+				// process action records
+				for (const record of records) {
+					getPluginService(strapi, 'publicationService').toggle(record, record.mode);
+				}
+			},
+		});
+	},
+};
