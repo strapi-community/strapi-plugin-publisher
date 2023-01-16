c7e9f092f1ec94001d33d4042da8c0f5def60124
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2023-01-15 22:28:40 -0500

    fix(routes): move settings routes to admin

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -1,8 +1,9 @@
 'use strict';
 
-const actionRoutes = require('./action');
+const actionRoutes = require('./action-routes');
+const settingsRoutes = require('./settings-routes');
 
 module.exports = {
 	type: 'admin',
-	routes: [...actionRoutes],
+	routes: [...actionRoutes, settingsRoutes],
 };
