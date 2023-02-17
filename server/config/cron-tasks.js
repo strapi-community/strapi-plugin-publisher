93f67cb9532a4637bc4273e8958f3206f02ef6d7
Cameron Paczek <cpazek@gmail.com>
2023-02-16 19:11:46 -0800

    convert cron to data format

--- server/config/cron-tasks.js
+++ server/config/cron-tasks.js
@@ -8,21 +8,26 @@ module.exports = {
 
 		// create cron check
 		strapi.cron.add({
-			[settings.actions.syncFrequency]: async () => {
-				// fetch all actions that have passed
-				const records = await getPluginService('action').find({
-					filters: {
-						executeAt: {
-							$lte: Date.now(),
+			publisherCronTask: {
+				options: {
+					rule: settings.actions.syncFrequency
+				},
+				task: async () => {
+					// fetch all actions that have passed
+					const records = await getPluginService('action').find({
+						filters: {
+							executeAt: {
+								$lte: Date.now(),
+							},
 						},
-					},
-				});
+					});
 
-				// process action records
-				for (const record of records.results) {
-					getPluginService('publicationService').toggle(record, record.mode);
-				}
-			},
+					// process action records
+					for (const record of records.results) {
+						getPluginService('publicationService').toggle(record, record.mode);
+					}
+				},
+			}
 		});
 	},
 };
