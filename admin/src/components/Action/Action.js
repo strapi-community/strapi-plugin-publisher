5ff53395726ccd72785d03756d50d88bc66f4040
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2023-08-23 21:40:03 -0400

    fix(publish): ensure required fields are set before saving publish date (#100)
    Resolves #89

--- admin/src/components/Action/Action.js
+++ admin/src/components/Action/Action.js
@@ -1,19 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import { useRBAC } from '@strapi/helper-plugin';
+import { createYupSchema } from '@strapi/admin/admin/src/content-manager/utils';
+import { useRBAC, useCMEditViewDataManager, useNotification } from '@strapi/helper-plugin';
 import PropTypes from 'prop-types';
 import { usePublisher } from '../../hooks/usePublisher';
 import { Stack } from '@strapi/design-system/Stack';
 import ActionTimePicker from './ActionDateTimePicker';
 import ActionButtons from './ActionButtons/ActionButtons';
+import { ValidationError } from 'yup';
+import { getTrad } from '../../utils/getTrad';
 
 const Action = ({ mode, entityId, entitySlug }) => {
 	const { createAction, getAction, updateAction, deleteAction } = usePublisher();
+	const entity = useCMEditViewDataManager();
+	const toggleNotification = useNotification();
 	const [actionId, setActionId] = useState(0);
 	const [isEditing, setIsEditing] = useState(false);
 	const [executeAt, setExecuteAt] = useState(0);
 	const [isCreating, setIsCreating] = useState(false);
 	const [isLoading, setIsLoading] = useState(false);
 	const [canPublish, setCanPublish] = useState(true);
+
+	let schema;
+	if (mode === 'publish') {
+		const currentContentTypeLayout = entity.allLayoutData.contentType || {};
+		schema = createYupSchema(
+			currentContentTypeLayout,
+			{ components: entity.allLayoutData.components || {} },
+			{ isCreatingEntry: entity.isCreatingEntry, isDraft: false, isFromComponent: false }
+		);
+	}
 
 	const { isLoading: isLoadingPermissions, allowedActions } = useRBAC({
 		publish: [{ action: 'plugin::content-manager.explorer.publish', subject: entitySlug }],
@@ -67,6 +82,10 @@ const Action = ({ mode, entityId, entitySlug }) => {
 	async function handleOnSave() {
 		setIsLoading(true);
 		try {
+			if (mode === 'publish' && schema) {
+				await schema.validate(entity.initialData, { abortEarly: false });
+			}
+
 			if (!actionId) {
 				await createAction({
 					mode,
@@ -81,6 +100,15 @@ const Action = ({ mode, entityId, entitySlug }) => {
 			setIsCreating(false);
 			setIsEditing(true);
 		} catch (error) {
+			if (error instanceof ValidationError) {
+				toggleNotification({
+					type: 'warning',
+					message: {
+						id: getTrad('action.notification.publish.validation.error'),
+						defaultMessage: 'Required fields must be saved before a publish date can be set',
+					},
+				});
+			}
 			console.error(error);
 		} finally {
 			setIsLoading(false);
