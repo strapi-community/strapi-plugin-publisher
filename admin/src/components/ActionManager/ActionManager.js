5342e2e799fdacd790d70c85286837ce003612bc
Tasos Vakirtzis <a.vakirtzis@wellcomm.gr>
2023-11-09 12:43:38 +0200

    add functionality to exclude content types

--- admin/src/components/ActionManager/ActionManager.js
+++ admin/src/components/ActionManager/ActionManager.js
@@ -4,18 +4,25 @@ import { useCMEditViewDataManager } from '@strapi/helper-plugin';
 import { getTrad } from '../../utils/getTrad';
 import { Box, Stack, Typography, Divider } from '@strapi/design-system';
 import Action from '../Action';
+import { useSettings } from 'strapi-plugin-publisher/admin/src/hooks/useSettings';
 
 const actionModes = ['publish', 'unpublish'];
 
 const ActionManager = () => {
 	const { formatMessage } = useIntl();
 	const entity = useCMEditViewDataManager();
+	const { getSettings } = useSettings();
+	const { data } = getSettings();
 
 	if (!entity.hasDraftAndPublish || entity.isCreatingEntry) {
 		return null;
 	}
 
 	if (!entity.modifiedData?.id) {
+		return null;
+	}
+
+	if(data && data.contentTypesToExclude && data.contentTypesToExclude[entity.slug]) {
 		return null;
 	}
 
